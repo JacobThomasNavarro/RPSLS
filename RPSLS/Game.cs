0e78f4485f17730567b67fcc2f51f365e5871e27
Jake Navarro <jt10navarro@gmail.com>
2019-09-25 13:55:46 -0500

    adding classes w/ basic functions

---
+++ RPSLS/Game.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    class Game
+    {
+        // member variables (HAS A)
+        public ComputerPlayer playerOne;
+        public HumanPlayer playerTwo;
+        public string RPSLS;
+        public Random rng;
+
+        // constructor
+        public Game()
+        {
+
+        }
+
+        // member methods (CAN DO)
+        public void RunGame() // master method
+        {
+
+        }
+        public void DisplayRules()
+        {
+            Console.WriteLine("Two players will throw Rock, Paper, Scissors, Spock or Lizard simultaneously.");
+            Console.WriteLine("Best 2 out of 3 will win!");
+        }
+        public void DisplayCurrentScore()
+        {
+            Console.WriteLine("Current score: " + playerOne.score + " " + playerTwo.score);
+        }
+        public void DisplayGameWinner()
+        {
+            if(playerOne.score > playerTwo.score)
+            {
+                Console.WriteLine("Player One won the game!");
+            }
+            else
+            {
+                Console.WriteLine("Player Two won the game!");
+            }
+        }
+    }
+}
---
+++ RPSLS/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    public abstract class Player
+    {
+        // member variables (HAS A)
+        public int score;
+
+        // constructor
+        public Player()
+        {
+            score = 0;
+        }
+
+        // member methods (CAN DO)
+        public abstract void MakeMove(); 
+    }
+}
